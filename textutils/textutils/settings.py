fa8b5750c2cd64eefcd25ec3f187584aa9f7eb47
2002sarika <71333456+2002sarika@users.noreply.github.com>
2022-02-27 16:41:52 +0530

    Update settings.py

--- textutils/textutils/settings.py
+++ textutils/textutils/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+x5-vr8g#ce+6egx8sr9wq%1ipl4=5j&o!iabm(*z29i_+(ius'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
